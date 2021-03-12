9582c06f5b4ed8ad7d05ff88bd7395ecec0c3da2
Luis <luis_peralta_000@hotmail.com>
2021-03-12 15:09:21 -0300

    actualizacion37-con-PSWP

--- js/swiper-prueba.js
+++ js/swiper-prueba.js
@@ -1,21 +1,21 @@
-(function($) {
+(function ($) {
     var $pswp = $('.pswp')[0];
     var image = [];
 
-    $('.swiper-wrapper').each( function() {
-        var $pic     = $(this),
-            getItems = function() {
+    $('.swiper-wrapper').each(function () {
+        var $pic = $(this),
+            getItems = function () {
                 var items = [];
-                $pic.find('img').each(function() {
-                    var $href   = $(this).attr('src'),
-                        $size   = $(this).data('size').split('x'),
-                        $width  = $size[0],
+                $pic.find('img').each(function () {
+                    var $href = $(this).attr('src'),
+                        $size = $(this).data('size').split('x'),
+                        $width = $size[0],
                         $height = $size[1];
 
                     var item = {
-                        src : $href,
-                        w   : $width,
-                        h   : $height
+                        src: $href,
+                        w: $width,
+                        h: $height
                     }
 
                     items.push(item);
@@ -25,17 +25,21 @@
 
         var items = getItems();
 
-        $.each(items, function(index, value) {
-            image[index]     = new Image();
+
+        $.each(items, function (index, value) {
+            image[index] = new Image();
             image[index].src = value['src'];
-            console.log(image[index]);
         });
 
-        $pic.on('click', 'figure', function(event) {
+
+        $pic.on('click', 'figure', function (event) {
             event.preventDefault();
-            
-            var $index = $(this).index();
-            console.log($index);
+
+            //para obtener el valor
+            var indice = parseInt($('.swiper-slide-active').attr("id"));
+            var $index = indice
+            console.log($index)
+
             var options = {
                 index: $index,
                 bgOpacity: 0.7,
